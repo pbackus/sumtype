9e7870ebb34d3aac95909f24bea965837515e71a
Paul Backus <snarwin@gmail.com>
2018-03-31 15:41:08 -0400

    First rough draft

---
+++ sumtype.d
@@ -0,0 +1,74 @@
+module sumtype;
+
+import std.variant: maxSize;
+import std.meta: staticIndexOf;
+import std.format: format;
+
+
+struct SumType(AllowedTypes...)
+{
+private:
+	// Type tag
+	size_t type;
+
+	// Value
+	enum valueName(T) = "value%d".format(staticIndexOf!(T, AllowedTypes));
+	union
+	{
+		static foreach (T; AllowedTypes) {
+			mixin(T.stringof ~ " " ~ valueName!T ~ ";");
+		}
+	}
+
+public:
+	static foreach (i, T; AllowedTypes) {
+		this(T val)
+		{
+			type = i;
+			mixin(valueName!T) = val;
+		}
+	}
+
+	static foreach (i, T; AllowedTypes) {
+		void opAssign(T rhs)
+		{
+			type = i;
+			mixin(valueName!T) = rhs;
+		}
+	}
+
+	auto match(handlers...)()
+	{
+		import std.traits: Parameters;
+
+		dispatch:
+		switch (type) {
+			static foreach (i, T; AllowedTypes)
+				static foreach (h; handlers)
+					static if (is(T == Parameters!h[0])) {
+						case i:
+							return h(mixin(valueName!T));
+							break dispatch;
+					}
+
+			default: throw new Exception("Missing handler");
+		}
+		assert(false); // unreached
+	}
+}
+
+unittest {
+	alias Foo = SumType!(int, float);
+	Foo x = Foo(42);
+	Foo y = Foo(3.14);
+
+	assert(x.match!((int v) => true, (float v) => false));
+	assert(y.match!((int v) => false, (float v) => true));
+}
+
+unittest {
+	alias Foo = SumType!(int, float);
+	Foo x = Foo(42);
+	x = 3.14;
+	assert(x.match!((float v) => true, (int v) => false));
+}
