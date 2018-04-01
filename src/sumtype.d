3715df5fafd65ecb7e658aaba883b2d486af0308
Paul Backus <snarwin@gmail.com>
2018-03-31 22:11:45 -0400

    Add generic handler support

--- src/sumtype.d
+++ src/sumtype.d
@@ -39,26 +39,51 @@ public:
 	}
 }
 
+import std.traits: Parameters, Unqual;
+
+private enum isHandlerFor(T, alias h) =
+	is(typeof(h(T.init))) &&
+	is(Unqual!T == Unqual!(Parameters!h[0]));
+
+private enum isGenericHandlerFor(T, alias h) =
+	is(typeof(h!T(T.init)));
+
 template match(handlers...)
 {
 	auto match(Self : SumType!Types, Types...)(Self self)
 	{
-		pure static int[Types.length] getHandlerIndices()
+		pure static auto getHandlerIndices()
 		{
-			import std.traits: Parameters, Unqual;
+			import std.traits: isCallable;
 
-			int[Types.length] result;
+			struct Result
+			{
+				int[Types.length] indices;
+				int[Types.length] generic;
 
-			foreach (i, T; Types) {
+				alias indices this;
+			}
+
+			Result result;
+
+			static foreach (i, T; Types) {
 				result[i] = -1;
-				foreach (j, h; handlers) {
-					if (is(typeof(h(T.init))) &&
-					    is(Unqual!T == Unqual!(Parameters!h[0]))) {
+				result.generic[i] = -1;
+
+				static foreach (j, h; handlers) {
+					static if (isCallable!h && isHandlerFor!(T, h)) {
 						if (result[i] == -1) {
 							result[i] = j;
 						} else {
 							assert(false,
-								"multiple handlers given for type " ~ T.stringof);
+									"multiple handlers given for type " ~ T.stringof);
+						}
+					} else static if (isGenericHandlerFor!(T, h)) {
+						if (result.generic[i] == -1) {
+							result.generic[i] = j;
+						} else {
+							assert(false,
+								"multiple generic handlers match for type " ~ T.stringof);
 						}
 					}
 				}
@@ -79,6 +104,9 @@ template match(handlers...)
 				static if (handlerIndices[i] != -1) {
 					case i:
 						return handlers[handlerIndices[i]](mixin("self." ~ valueName!T));
+				} else static if (handlerIndices.generic[i] != -1) {
+					case i:
+						return handlers[handlerIndices.generic[i]](mixin("self." ~ valueName!T));
 				} else {
 					static assert(false, "missing handler for type " ~ T.stringof);
 				}
@@ -131,4 +159,26 @@ unittest {
 	Foo y = Foo(3.14);
 	assert(x.match!((int v) => v == answer, (float v) => v == answer));
 	assert(!y.match!((int v) => v == answer, (float v) => v == answer));
+}
+
+// Generic handler
+unittest {
+	import std.math: approxEqual;
+
+	alias Foo = SumType!(int, float);
+	Foo x = Foo(42);
+	Foo y = Foo(3.14);
+	assert(x.match!(v => v*2) == 84);
+	assert(y.match!(v => v*2).approxEqual(6.28));
+}
+
+// Fallback to generic handler
+unittest {
+	import std.conv: to;
+
+	alias Foo = SumType!(int, float, string);
+	Foo x = Foo(42);
+	Foo y = Foo("42");
+	assert(x.match!((string v) => v.to!int, v => v*2) == 84);
+	assert(y.match!((string v) => v.to!int, v => v*2) == 42);
 }
