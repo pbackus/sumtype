2d22900123e49f90b792e04a5d8ddc1cbf7553c3
Paul Backus <snarwin@gmail.com>
2018-04-04 22:00:36 -0400

    Eliminate unused loop index

--- src/sumtype.d
+++ src/sumtype.d
@@ -181,26 +181,26 @@ template match(handlers...)
 
 			indices[] = -1;
 
-			static foreach (i, T; Types) {
-				static foreach (j, h; handlers) {
+			static foreach (T; Types) {
+				static foreach (i, h; handlers) {
 					// Regular handlers
 					static if (isCallable!h && is(typeof(h(T.init)))) {
 						// Functions and delegates
 						static if (isSomeFunction!h) {
 							static if (sameUnqual!(T, Parameters!h[0])) {
-								setHandlerIndex!T(j);
+								setHandlerIndex!T(i);
 							}
 						// Objects with overloaded opCall
 						} else static if (hasMember!(typeof(h), "opCall")) {
 							static foreach (overload; __traits(getOverloads, typeof(h), "opCall")) {
 								static if (sameUnqual!(T, Parameters!overload[0])) {
-									setHandlerIndex!T(j);
+									setHandlerIndex!T(i);
 								}
 							}
 						}
 					// Generic handlers
 					} else static if (is(typeof(h!T(T.init)))) {
-						setHandlerIndex!T(j);
+						setHandlerIndex!T(i);
 					}
 				}
 			}
