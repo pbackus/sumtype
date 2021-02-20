2a4b96cbe855e92ba8a818646682c534b0263423
Paul Backus <snarwin@gmail.com>
2021-02-20 14:16:08 -0500

    Explain purpose of toCtString unittest in comment

--- src/sumtype.d
+++ src/sumtype.d
@@ -236,6 +236,8 @@ struct This {}
 // Converts an unsigned integer to a compile-time string constant.
 private enum toCtString(ulong n) = n.stringof[0 .. $ - "LU".length];
 
+// Check that .stringof does what we expect, since it's not guaranteed by the
+// lanugage spec.
 @safe unittest {
 	assert(toCtString!0 == "0");
 	assert(toCtString!123456 == "123456");
