pbackus/sumtype

commit 88b527bc25365646369422306508599c9534989c

Avoid generating lots of small temporary functions.
- Move destructor lambda into global scope (allow instance reuse)
- Move `stride` template into global scope (allow instance reuse)
- `getValues` yields a string of an argument list instead of a value tuple.

Effects:

- slightly slower in normal operation
- enormously faster in degenerate cases (large number of types, `-O -inline`) due to avoiding overloading the inliner
- smaller object file size.

src/sumtype.d
@@ -509,11 +509,7 @@ public:
 					cast(void) () @system {}();
 				}
 
-				this.match!((ref value) {
-					static if (hasElaborateDestructor!(typeof(value))) {
-						destroy(value);
-					}
-				});
+				this.match!(.maybeDestroyValue);
 
 				mixin("Storage newStorage = { ",
 					Storage.memberName!T, ": forward!rhs",
@@ -1310,6 +1306,8 @@ enum bool isSumType(T) = is(T : SumType!Args, Args...);
 	assert(!isSumType!ContainsSumType);
 }
 
+private enum size_t sumTypeLength(S) = S.Types.length;
+
 /**
  * Calls a type-appropriate function with the value held in a [SumType].
  *
@@ -1561,36 +1559,38 @@ private template Iota(size_t n)
 	assert(Iota!3 == AliasSeq!(0, 1, 2));
 }
 
+/* The number that the dim-th argument's tag is multiplied by when
+ * converting TagTuples to and from case indices ("caseIds").
+ *
+ * Named by analogy to the stride that the dim-th index into a
+ * multidimensional static array is multiplied by to calculate the
+ * offset of a specific element.
+ */
+private size_t stride(size_t dim, length...)()
+{
+	import core.checkedint: mulu;
+
+	size_t result = 1;
+	bool overflow = false;
+
+	static foreach (i; 0 .. dim) {
+		result = mulu(result, length[i], overflow);
+	}
+
+	/* The largest number matchImpl uses, numCases, is calculated with
+	 * stride!(SumTypes.length), so as long as this overflow check
+	 * passes, we don't need to check for overflow anywhere else.
+	 */
+	assert(!overflow, "Integer overflow");
+	return result;
+}
+
 private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
 {
-	auto ref matchImpl(SumTypes...)(auto ref SumTypes args)
+	auto matchImpl(SumTypes...)(auto ref SumTypes args)
 		if (allSatisfy!(isSumType, SumTypes) && args.length > 0)
 	{
-		/* The number that the dim-th argument's tag is multiplied by when
-		 * converting TagTuples to and from case indices ("caseIds").
-		 *
-		 * Named by analogy to the stride that the dim-th index into a
-		 * multidimensional static array is multiplied by to calculate the
-		 * offset of a specific element.
-		 */
-		static size_t stride(size_t dim)()
-		{
-			import core.checkedint: mulu;
-
-			size_t result = 1;
-			bool overflow = false;
-
-			static foreach (S; SumTypes[0 .. dim]) {
-				result = mulu(result, S.Types.length, overflow);
-			}
-
-			/* The largest number matchImpl uses, numCases, is calculated with
-			 * stride!(SumTypes.length), so as long as this overflow check
-			 * passes, we don't need to check for overflow anywhere else.
-			 */
-			assert(!overflow, "Integer overflow");
-			return result;
-		}
+		alias stride(size_t i) = .stride!(i, Map!(sumTypeLength, SumTypes));
 
 		/* A TagTuple represents a single possible set of tags that `args`
 		 * could have at runtime.
@@ -1662,26 +1662,17 @@ private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
 			}
 		}
 
-		/* An AliasSeq of zero-argument functions that return, by ref, the
-		 * member values of `args` needed for the case labeled with `caseId`.
-		 *
-		 * When used in an expression context (like, say, a function call), it
-		 * will instead be interpreted as a sequence of zero-argument function
-		 * *calls*, with optional parentheses omitted.
+		/*
+		 * A list of arguments to be passed to a handler needed for the case
+		 * labeled with `caseId`.
 		 */
 		template values(size_t caseId)
 		{
 			enum tags = TagTuple.fromCaseId(caseId);
-
-			// Workaround for dlang issue 21348
-			ref getValue(size_t i)(ref SumTypes[i] arg = args[i])
-			{
-				enum tid = tags[i];
-				alias T = SumTypes[i].Types[tid];
-				return arg.get!T;
-			}
-
-			alias values = Map!(getValue, Iota!(tags.length));
+			enum getValue(size_t i: tags.length) = "";
+			enum getValue(size_t i) = "args[" ~ toCtString!i ~ "].get!(SumTypes[" ~ toCtString!i ~ "]" ~
+				".Types[" ~ toCtString!(tags[i]) ~ "])(), " ~ getValue!(i + 1);
+			enum values = getValue!0;
 		}
 
 		/* An AliasSeq of the types of the member values returned by the
@@ -1775,7 +1766,7 @@ private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
 			static foreach (caseId; 0 .. numCases) {
 				case caseId:
 					static if (matches[caseId] != noMatch) {
-						return mixin(handlerName!(matches[caseId]))(values!caseId);
+						return mixin(handlerName!(matches[caseId]) ~ "(" ~ values!caseId ~ ")");
 					} else {
 						static if(exhaustive) {
 							static assert(false,
@@ -2226,4 +2217,11 @@ static if (__traits(compiles, { import std.traits: isRvalueAssignable; })) {
 	struct __InoutWorkaroundStruct{}
 	@property T rvalueOf(T)(inout __InoutWorkaroundStruct = __InoutWorkaroundStruct.init);
 	@property ref T lvalueOf(T)(inout __InoutWorkaroundStruct = __InoutWorkaroundStruct.init);
+}
+
+private void maybeDestroyValue(T)(ref T value)
+{
+	static if (hasElaborateDestructor!T) {
+		destroy(value);
+	}
 }

commit 7fd8c00ae745111ba2e5adef527332d7162de068

Make matchImpl return auto ref again
Fixes #65 on Github, a regression introduced in 88b527bc (Github PR #50)

src/sumtype.d
@@ -1679,7 +1679,7 @@ private size_t stride(size_t dim, lengths...)()
 
 private template matchImpl(Flag!"exhaustive" exhaustive, handlers...)
 {
-	auto matchImpl(SumTypes...)(auto ref SumTypes args)
+	auto ref matchImpl(SumTypes...)(auto ref SumTypes args)
 		if (allSatisfy!(isSumType, SumTypes) && args.length > 0)
 	{
 		enum typeCount(SumType) = SumType.Types.length;
@@ -2109,6 +2109,15 @@ version (D_Exceptions)
 	);
 
 	assert(value.get!double.isClose(6.28));
+}
+
+// Handlers that return by ref
+@safe unittest {
+	SumType!int x = 123;
+
+	x.match!(ref (ref int n) => n) = 456;
+
+	assert(x.match!((int n) => n == 456));
 }
 
 // Unreachable handlers